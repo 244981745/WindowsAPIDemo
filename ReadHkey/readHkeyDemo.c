3e0e2834d5b66fa677e8142b148c08d7da888f85
244981745 <37541600+244981745@users.noreply.github.com>
2021-03-03 22:28:18 +0800

    bugfix

--- ReadHkey/readHkeyDemo.c
+++ ReadHkey/readHkeyDemo.c
@@ -15,11 +15,12 @@ int main(int argc, char **agrv)
 {
 	int		i;
 	HKEY	hkey = NULL;
-	long	lRet;
+	long	lRet,ls;
 	DWORD	value_total = 0;
-	DWORD	dwSize=256;
-	LPWSTR	name[MAX_PATH];
+	DWORD	dwSize;
+	char	name[MAX_PATH];
 	char	data[MAX_PATH];
+	
 	
 	lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "HARDWARE\\DEVICEMAP\\SERIALCOMM", 0, KEY_QUERY_VALUE, &hkey);
 	if(lRet == ERROR_SUCCESS)
@@ -34,11 +35,17 @@ int main(int argc, char **agrv)
 			printf("Query Info Success,Total:%d\n",value_total);
 			for(i = 0; i < value_total; i++)
 			{
+				memset((void*)name,'\0',256);
+				memset((void*)data,'\0',256);
+				dwSize	=	256;
 				lRet	=	RegEnumValue(hkey, i, name, &dwSize, NULL, NULL, NULL, NULL);
 				if(lRet	== ERROR_SUCCESS)
-					lRet	=	RegQueryValueEx(hkey,name,NULL,NULL,(LPBYTE)data,&dwSize);
-					if(lRet	== ERROR_SUCCESS)
+				{
+					dwSize	=	256;
+					ls	=	RegQueryValueEx(hkey,name,NULL,NULL,(LPBYTE)data,&dwSize);
+					if(ls	== ERROR_SUCCESS)
 						printf("%s,Len:%d\n",data,dwSize);
+				}
 			}
 
 		}
